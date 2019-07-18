44785a3bad513c76c356ad30e43ac2ab49fda45f
keen <kyh0927@within.co.kr>
2019-07-18 16:32:40 +0900

    TEST

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import './styles/main.scss';
 
 import Home from './pages/Home';
+import SearchHome from './pages/SearchHome';
 
 export class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/" component={SearchHome} />
         </Switch>
       </div>
     )
---
+++ src/pages/SearchHome/SearchHome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchHome() {
+    return (
+        <div>
+            검색홈
+        </div>
+    )
+}
+
+export default SearchHome
